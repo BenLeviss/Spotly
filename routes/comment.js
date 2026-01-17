e85ab1bad96fc652ba97910379d0d14d166f5e35
Avia Naftali <avianaftali@Avias-MacBook-Air.local>
2026-01-17 13:44:51 +0200

    comment Router

--- routes/comment.js
+++ routes/comment.js
@@ -2,8 +2,5 @@ const express = require("express");
 const commentController = require("../controllers/comment");
 const commentRouter = express.Router();
 
-commentRouter.post("/", (res, req) => {
-    commentController.addComment(res, req)
-});
 
 module.exports = commentRouter; 
--- routes/posts.js
+++ routes/posts.js
@@ -1,20 +1,20 @@
 const express = require("express");
-const router = express.Router();
+const postRouter = express.Router();
 const postsController = require("../controllers/posts");
 
 // 1. Add a New Post
-router.post("/", postsController.createPost);
+postRouter.post("/", postsController.createPost);
 
 // 2. Posts All Posts / By sender
-router.get("/", postsController.getPosts);
+postRouter.get("/", postsController.getPosts);
 
 // 3. Get a Post by ID
-router.get("/:id", postsController.getPostById);
+postRouter.get("/:id", postsController.getPostById);
 
 // 4. Update a Post
-router.put("/:id", postsController.updatePostById);
+postRouter.put("/:id", postsController.updatePostById);
 
 // 5. Delete a Post by ID
-router.delete("/:id", postsController.deletePostById);
+postRouter.delete("/:id", postsController.deletePostById);
 
-module.exports = router;
+module.exports = postRouter;
