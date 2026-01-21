04f8fccb2e5785922eda1dcbfe8c76dc234a21c4
Avia Naftali <avianaftali@Avias-MacBook-Air.local>
2026-01-21 13:08:56 +0200

    fix

--- routes/comment.js
+++ routes/comment.js
@@ -2,9 +2,16 @@ const express = require("express");
 const commentRouter = express.Router();
 const commentController = require("../controllers/comment");
 
+// Get a All Comments
 commentRouter.get("/", commentController.getAllComments);
+
+// Get a Comment by ID
 commentRouter.get("/:commentId", commentController.getCommentById);
+
+// Update a Comment by ID
 commentRouter.put("/:commentId", commentController.updateCommentById);
+
+// Delete a Comment by ID
 commentRouter.delete("/:commentId", commentController.deleteCommentById);
 
 module.exports = commentRouter; 
